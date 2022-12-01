7fd14bbe15f58fd660d220961ea8cda807cfa608
ebrahimkhodadadi <ebrahimkhodadadideveloper@gmail.com>
2022-12-01 14:45:28 +0330

    Add Entities

---
+++ MauiApplication/Platforms/Android/MainApp.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Android.Runtime;
+
+namespace MauiApp.Platforms.Android
+{
+    [Application]
+    public class MainApp : MauiApplication
+    {
+        public MainApp(IntPtr handle, JniHandleOwnership ownership)
+            : base(handle, ownership)
+        {
+        }
+
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
